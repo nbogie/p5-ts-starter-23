db929a7a21a29ebc54f76e8d421a725c03afe76b
nbogie <neillbogie@googlemail.com>
2022-10-01 01:18:42 +0100

    add p5 and its types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-document.body.innerHTML = "Hello parcel is doing its thing!";
+import p5 from "p5";
+console.log("imported p5");
+const sketch = function (p) {
+    function setup() {
+        console.log("setup called");
+        p.createCanvas(p.windowWidth, p.windowHeight);
+    }
+
+    function draw() {
+        p.fill(p.random(255));
+        p.circle(p.random(p.width), p.random(p.height), p.random(10, 40));
+    }
+    p.setup = setup;
+    p.draw = draw;
+};
+const myP5 = new p5(sketch);
