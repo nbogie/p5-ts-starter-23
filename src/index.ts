e746f4939cb446728c709d3ae89b7f1c94fa8db9
nbogie <neillbogie@googlemail.com>
2022-10-02 01:57:01 +0100

    not yet working

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,34 @@
 import p5 from "p5";
+import "p5/lib/addons/p5.sound";
+// /* @ts-ignore */
+// window.p5 = p5;
+
+let fft: p5.FFT;
 
 const sketch = function (p: p5) {
     function setup() {
         const myCanvas = p.createCanvas(p.windowWidth, p.windowHeight);
         myCanvas.mousePressed(handleMousePressed);
-        // console.log("Setup ran.");
-        // p.noLoop();
+        console.log(p5);
+        let mic = new p5.AudioIn();
+        mic.start();
+        fft = new p5.FFT();
+        fft.setInput(mic);
     }
 
     function draw() {
+        fft.analyze(); // Compute the energy levels for each frequency band.
+
+        const maxFreq = 2000; //Hz
+        const numStepsOnXAxis = 100;
+        const freqStep = maxFreq / numStepsOnXAxis;
+        for (let freq = 40; freq < maxFreq; freq += freqStep) {
+            const energyLevel = fft.getEnergy(freq);
+            const y = p.map(energyLevel, 0, 255, 1000, 0); //convert energy from 0<->255 to 1000<->0
+            const x = p.map(freq, 0, maxFreq, 0, p.width); //convert freq from 0<->maxFreq to 0<->width
+
+            p.circle(x, y, 16);
+        }
         drawARandomCircle();
     }
 
