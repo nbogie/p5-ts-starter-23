5db131673c76bc8eb76f05573e5367fbe9183b57
nbogie <neillbogie@googlemail.com>
2022-11-14 21:03:20 +0000

    tidy sketch setup

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,32 @@
 import p5 from "p5";
 
-const sketch = function (p: p5) {
+const myP5 = new p5(createSketch);
+
+function createSketch(p: p5) {
+
+
+    //Crucially, assign the setup and draw functions for the p5 createSketch.
+    p.setup = setup;
+    p.draw = draw;
+
     function setup() {
         const myCanvas = p.createCanvas(p.windowWidth, p.windowHeight);
+
         myCanvas.mousePressed(handleMousePressed);
-        // console.log("Setup ran.");
+
         // p.noLoop();
     }
+
 
     function draw() {
         drawARandomCircle();
     }
 
+
     function drawARandomCircle() {
         const colour = getRandomColour();
         p.fill(colour);
+
         p.noStroke();
         const diameter = p.random(20, 100);
         p.circle(p.mouseX, p.mouseY, diameter);
@@ -29,10 +41,10 @@ const sketch = function (p: p5) {
         p.background("white");
     }
 
-    //Crucially, assign the setup and draw functions for the p5 sketch.
-    p.setup = setup;
-    p.draw = draw;
 
     // p.windowResized = () => p.resizeCanvas(p.windowWidth, p.windowHeight);
 };
-const myP5 = new p5(sketch);
+
+
+
+
