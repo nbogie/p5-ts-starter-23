329c071232fe967ef317f60180dd11d7b6fa5b53
nbogie <neillbogie@googlemail.com>
2023-06-24 15:15:26 +0100

    formatting only ( w prettier )

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,41 @@
-import p5 from "p5";
+import p5 from 'p5'
 
-const myP5 = new p5(createSketch);
+const myP5 = new p5(createSketch)
 
 function createSketch(p: p5) {
-
-
     //Crucially, assign the setup and draw functions for the p5 createSketch.
-    p.setup = setup;
-    p.draw = draw;
+    p.setup = setup
+    p.draw = draw
 
     function setup() {
-        const myCanvas = p.createCanvas(p.windowWidth, p.windowHeight);
+        const myCanvas = p.createCanvas(p.windowWidth, p.windowHeight)
 
-        myCanvas.mousePressed(handleMousePressed);
+        myCanvas.mousePressed(handleMousePressed)
 
         // p.noLoop();
     }
 
-
     function draw() {
-        drawARandomCircle();
+        drawARandomCircle()
     }
 
-
     function drawARandomCircle() {
-        const colour = getRandomColour();
-        p.fill(colour);
+        const colour = getRandomColour()
+        p.fill(colour)
 
-        p.noStroke();
-        const diameter = p.random(20, 100);
-        p.circle(p.mouseX, p.mouseY, diameter);
+        p.noStroke()
+        const diameter = p.random(20, 100)
+        p.circle(p.mouseX, p.mouseY, diameter)
     }
 
     function getRandomColour() {
-        const palette = ["#556270", "#4ecdc4", "#c7f464", "#ff6b6b", "#c44d58"];
-        return p.random(palette);
+        const palette = ['#556270', '#4ecdc4', '#c7f464', '#ff6b6b', '#c44d58']
+        return p.random(palette)
     }
 
     function handleMousePressed() {
-        p.background("white");
+        p.background('white')
     }
 
-
     // p.windowResized = () => p.resizeCanvas(p.windowWidth, p.windowHeight);
-};
-
-
-
-
+}
--- src/utils/partition.ts
+++ src/utils/partition.ts
@@ -1,13 +1,13 @@
 function partition<T>(arr: T[], predFn: (arg: T) => boolean): [T[], T[]] {
-    const a: T[] = [];
-    const b: T[] = [];
+    const a: T[] = []
+    const b: T[] = []
     for (const el of arr) {
         if (predFn(el)) {
-            a.push(el);
+            a.push(el)
         } else {
-            b.push(el);
+            b.push(el)
         }
     }
-    return [a, b];
+    return [a, b]
 }
-export { partition };
+export { partition }
