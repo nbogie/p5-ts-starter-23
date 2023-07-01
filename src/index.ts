da0a18b2f24cfbf47ba0f8ca647594e055374ee1
nbogie <neillbogie@googlemail.com>
2023-07-01 11:25:58 +0100

    docs

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,13 @@ import { Walker, createWalker, drawWalker, updateWalker } from './walker'
 new p5(createSketch)
 
 function createSketch(p: p5) {
-    //In instance mode, your previously "global" variables likely can sit here (where they won't conflict with other loaded sketches)
-    let walker: Walker
+    //In instance mode your previously "global" variables can live here 
+    //(where they won't conflict with other loaded sketches)
 
+    /** A random-walker object */
+    let walker: Walker;
+
+    /** Called once, automatically, by p5.js */
     function setup() {
         const myCanvas = p.createCanvas(p.windowWidth, p.windowHeight)
         myCanvas.mousePressed(handleMousePressed)
@@ -14,19 +18,22 @@ function createSketch(p: p5) {
         // p.noLoop();
     }
 
+    /** Called every frame, automatically, by p5.js */
     function draw() {
         drawWalker(walker, p)
         updateWalker(walker, p)
     }
 
     function handleMousePressed() {
-        p.background('white')
-        walker.pos.x = p.mouseX
-        walker.pos.y = p.mouseY
+        if (p.mouseButton === p.LEFT) {
+            p.background('white');
+            walker.pos.x = p.mouseX;
+            walker.pos.y = p.mouseY;
+        }
     }
 
     //Crucially, assign the setup and draw functions for the p5 createSketch.
-    p.setup = setup
-    p.draw = draw
-    p.windowResized = () => p.resizeCanvas(p.windowWidth, p.windowHeight)
+    p.setup = setup;
+    p.draw = draw;
+    p.windowResized = () => p.resizeCanvas(p.windowWidth, p.windowHeight);
 }
--- src/walker.ts
+++ src/walker.ts
@@ -4,16 +4,19 @@ export interface Walker {
     pos: { x: number; y: number }
 }
 
+/** create a Walker at the given position. */
 export function createWalker(x: number, y: number, p: p5): Walker {
     return { pos: { x, y } }
 }
 
+/** Visualise the given Walker's current position and state */
 export function drawWalker(walker: Walker, p: p5) {
     p.fill(getRandomColour(p))
     p.rectMode(p.CENTER)
     p.square(walker.pos.x, walker.pos.y, 10)
 }
 
+/** Move and perhaps otherwise update the given Walker */
 export function updateWalker(walker: Walker, p: p5) {
     walker.pos.x += p.random(-10, 10)
     walker.pos.y += p.random(-10, 10)
