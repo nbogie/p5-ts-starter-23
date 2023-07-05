9999fc11a72793b2c69261ea3542971e1303a70f
nbogie <neillbogie@googlemail.com>
2023-07-05 20:19:17 +0100

    reorg sketches into subfolders

--- src/utils.ts
+++
@@ -1,9 +0,0 @@
-function collect<T>(num: number, creatorFn: (ix: number) => T): T[] {
-    const results: T[] = [];
-    for (let i = 0; i < num; i++) {
-        results.push(creatorFn(i));
-    }
-    return results;
-}
-
-export { collect };
